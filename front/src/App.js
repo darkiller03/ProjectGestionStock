7e5a7e0b5defb203a48d36efd86e19244d912236
Karim Hssini <hssinikarim2003@gmail.com>
2024-08-28 11:31:23 +0100

    fixed CRUD for Products

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from './dashboard/Dashboard';
-import Products from './dashboard/Product';
-import Order from './dashboard/Order';
-import Clients from './dashboard/Clients';
-import Suppliers from './dashboard/Suppliers';
-import Login from './login/LoginForm';
-import PrivateRoute from './PrivateRoute';
-import { isAuthenticated } from './auth';
+import React, { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./dashboard/Dashboard";
+import Products from "./dashboard/Product";
+import Order from "./dashboard/Order";
+import Clients from "./dashboard/Clients";
+import Suppliers from "./dashboard/Suppliers";
+import Login from "./login/LoginForm";
+import PrivateRoute from "./PrivateRoute";
+import RootLayout from "./layout/RootLayout";
+import { isAuthenticated } from "./auth";
+import ErrorRoute from "./components/ErrorRoute";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(isAuthenticated());
@@ -19,13 +21,29 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login onLogin={handleLogin} />} />
-      <Route path="/" element={isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
-      <Route path="/dashboard" element={<PrivateRoute element={Dashboard} />} />
-      <Route path="/products" element={<PrivateRoute element={Products} />} />
-      <Route path="/order" element={<PrivateRoute element={Order} />} />
-      <Route path="/clients" element={<PrivateRoute element={Clients} />} />
-      <Route path="/suppliers" element={<PrivateRoute element={Suppliers} />} />
-      <Route path="*" element={<Navigate to={isLoggedIn ? "/dashboard" : "/login"} />} />
+      <Route
+        path="/"
+        element={
+          isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+        }
+      />
+      <Route element={<PrivateRoute element={RootLayout} />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/suppliers" element={<Suppliers />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <ErrorRoute label="Dashboard" />
+          ) : (
+            <ErrorRoute label="Login" />
+          )
+        }
+      />
     </Routes>
   );
 }
